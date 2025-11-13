50a4cd8f5ec9501d8460477d7deba4deb0e2810b
realjunderwood <75750795+realjunderwood@users.noreply.github.com>
2025-11-13 00:07:53 -0500

    updated cypress test

--- tests/cypress/e2e/platform-api.cy.js
+++ tests/cypress/e2e/platform-api.cy.js
@@ -5,13 +5,37 @@ let token;
 
 const getUniqueId = () => { return Cypress._.uniqueId(Date.now().toString()); };
 const email = `${getUniqueId()}@test.com`;
+const password = 'password';
+
+const ensureToken = () => {
+  if (token) {
+    return cy.wrap(token);
+  }
+
+  return cy.request({
+    method: 'POST',
+    url: '/api/signup',
+    body: { email, password },
+    failOnStatusCode: false,
+  }).then(() => {
+    return cy.request({
+      method: 'POST',
+      url: '/api/signin',
+      body: { email, password },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      token = response.body.token;
+      return token;
+    });
+  });
+};
 
 describe('Platform-API WITH Authentication - ok to fail if you are on lab5 - use it.skip to skip these', () => {
   it('user signs up with email and password', () => {
     cy.request(
       'POST',
       '/api/signup',
-      { email, password: 'password' },
+      { email, password },
     ).then((response) => {
       expect(response.status).to.eq(200);
       token = response.body.token;
@@ -22,7 +46,7 @@ describe('Platform-API WITH Authentication - ok to fail if you are on lab5 - use
       failOnStatusCode: false,
       method: 'POST',
       url: '/api/signup',
-      body: { email, password: 'password' },
+      body: { email, password },
     }).then((response) => {
       expect(response.status).to.eq(422);
     });
@@ -31,7 +55,7 @@ describe('Platform-API WITH Authentication - ok to fail if you are on lab5 - use
     cy.request({
       method: 'POST',
       url: '/api/signin',
-      body: { email, password: 'password' },
+      body: { email, password },
     }).then((response) => {
       expect(response.status).to.eq(200);
       token = response.body.token;
@@ -53,29 +77,33 @@ describe('Platform-API WITH Authentication - ok to fail if you are on lab5 - use
     });
   });
   it('user makes post with auth token', () => {
-    cy.request({
-      method: 'POST',
-      url: '/api/posts',
-      headers: { authorization: token },
-      body: {
-        title: 'authenticated test post',
-        tags: 'words',
-        content: 'this is a test post',
-        coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.author).to.not.eq(undefined);
-      postId = response.body.id;
+    ensureToken().then(() => {
+      cy.request({
+        method: 'POST',
+        url: '/api/posts',
+        headers: { authorization: token },
+        body: {
+          title: 'authenticated test post',
+          tags: 'words',
+          content: 'this is a test post',
+          coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.author).to.not.eq(undefined);
+        postId = response.body.id;
+      });
     });
   });
   it('user deletes post with auth token', () => {
-    cy.request({
-      method: 'DELETE',
-      url: `/api/posts/${postId}`,
-      headers: { authorization: token },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
+    ensureToken().then(() => {
+      cy.request({
+        method: 'DELETE',
+        url: `/api/posts/${postId}`,
+        headers: { authorization: token },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
     });
   });
   it('user signs in with email and wrong password, expecting fail code 401', () => {
@@ -91,77 +119,93 @@ describe('Platform-API WITH Authentication - ok to fail if you are on lab5 - use
 });
 
 describe('Lab5: CRUD operations', () => {
+  before(() => {
+    ensureToken();
+  });
+
   it('user creates a post', () => {
-    cy.request({
-      method: 'POST',
-      headers: { authorization: token },
-      url: '/api/posts',
-      body:
-      {
-        title: 'first post',
-        tags: 'words',
-        content: 'this is a test post',
-        coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      postId = response.body.id;
+    ensureToken().then(() => {
+      cy.request({
+        method: 'POST',
+        headers: { authorization: token },
+        url: '/api/posts',
+        body:
+        {
+          title: 'first post',
+          tags: 'words',
+          content: 'this is a test post',
+          coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        postId = response.body.id;
+      });
     });
   });
   it('user retrieves a post', () => {
-    cy.request({
-      method: 'GET',
-      headers: { authorization: token },
-      url: `/api/posts/${postId}`,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.title).to.eq('first post');
+    ensureToken().then(() => {
+      cy.request({
+        method: 'GET',
+        headers: { authorization: token },
+        url: `/api/posts/${postId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.title).to.eq('first post');
+      });
     });
   });
   it('user retrieves a bad post id, expecting failure code 404', () => {
-    cy.request({
-      failOnStatusCode: false,
-      method: 'GET',
-      headers: { authorization: token },
-      url: '/api/posts/foobar',
-    }).then((response) => {
-      expect(response.status).to.eq(404);
+    ensureToken().then(() => {
+      cy.request({
+        failOnStatusCode: false,
+        method: 'GET',
+        headers: { authorization: token },
+        url: '/api/posts/foobar',
+      }).then((response) => {
+        expect(response.status).to.eq(404);
+      });
     });
   });
   it('user updates a post', () => {
-    cy.request({
-      method: 'PUT',
-      headers: { authorization: token },
-      url: `/api/posts/${postId}`,
-      body:
-      {
-        title: 'updated post',
-        tags: 'words',
-        content: 'this is a test post',
-        coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
-      },
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.title).to.eq('updated post');
+    ensureToken().then(() => {
+      cy.request({
+        method: 'PUT',
+        headers: { authorization: token },
+        url: `/api/posts/${postId}`,
+        body:
+        {
+          title: 'updated post',
+          tags: 'words',
+          content: 'this is a test post',
+          coverUrl: 'https://media.giphy.com/media/uscuTAPrWqmqI/giphy.gif',
+        },
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.title).to.eq('updated post');
+      });
     });
   });
   it('user deletes a post', () => {
-    cy.request({
-      method: 'DELETE',
-      headers: { authorization: token },
-      url: `/api/posts/${postId}`,
-    }).then((response) => {
-      expect(response.status).to.eq(200);
+    ensureToken().then(() => {
+      cy.request({
+        method: 'DELETE',
+        headers: { authorization: token },
+        url: `/api/posts/${postId}`,
+      }).then((response) => {
+        expect(response.status).to.eq(200);
+      });
     });
   });
   it('retrieving deleted post, expecting error code 404', () => {
-    cy.request({
-      failOnStatusCode: false,
-      method: 'GET',
-      headers: { authorization: token },
-      url: `/api/posts/${postId}`,
-    }).then((res) => {
-      expect(res.status).to.eq(404);
+    ensureToken().then(() => {
+      cy.request({
+        failOnStatusCode: false,
+        method: 'GET',
+        headers: { authorization: token },
+        url: `/api/posts/${postId}`,
+      }).then((res) => {
+        expect(res.status).to.eq(404);
+      });
     });
   });
 });
